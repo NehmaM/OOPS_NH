b1eb1c104d7b8d0f2ca6a63d2920dd2975064877
NehmaM <93966525+NehmaM@users.noreply.github.com>
2022-03-18 11:37:17 +0530

    Add files via upload

---
+++ Distance/Dist.java
@@ -0,0 +1,11 @@
+class Dist
+{
+	int meter;
+	double centim;
+
+	void getdata()
+	{
+		meter=10;
+		centim=25.5;
+	}
+}
---
+++ Distance/Distance.java
@@ -0,0 +1,10 @@
+class Distance
+{
+	public static void main(String args[])
+	{
+		Dist d=new Dist();		
+		d.getdata();
+		System.out.println("Meter="+d.meter);	
+		System.out.println("centimeter="+d.centim);
+	}
+}
